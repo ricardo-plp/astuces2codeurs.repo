4d752aa1570201e331d2044f5563bc48eca5a9f9
Ricardo <tchamgouericardo@gmail.com>
2023-07-30 00:08:47 +0200

    setting admin panel for the post model

--- blog/admin.py
+++ blog/admin.py
@@ -3,4 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from blog.models import Post
 
-admin.site.register(Post)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','status','Author','created')
+    prepopulated_fields = {'slug':('title',)}
+    search_fields = ('title','body','author')
+    ordering = ('created','title','Author')
